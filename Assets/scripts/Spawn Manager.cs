d43b17c17211956a6ce4b00976bf38486080c7c2
flexusISF <146860748+flexusISF@users.noreply.github.com>
2023-10-03 10:18:09 -0700

    3.1 added blender suziebot

--- Assets/scripts/Spawn Manager.cs
+++ Assets/scripts/Spawn Manager.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject obstablePrefab;
+    private Vector3 Spawnpos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
-    void Update()
+    void SpawnObstacle ()
     {
-        
+        Instantiate(obstablePrefab, Spawnpos, obstablePrefab.transform.rotation);
     }
 }
